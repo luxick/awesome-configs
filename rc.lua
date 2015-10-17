88331349c031fe1aee0f2ee4aac1f23f7cf7ae44
luxick <git@luxick.com>
2015-10-17 16:49:27 +0200

    Switched from lain to vain for special layouts

--- rc.lua
+++ rc.lua
@@ -6,7 +6,7 @@ awful.util      = require("awful.util")
 beautiful       = require("beautiful")
 naughty         = require("naughty")
 lain            = require("lain")
---vain            = require("vain")
+vain            = require("vain")
 vicious         = require("vicious")
 blingbling		= require("blingbling")
 
@@ -18,6 +18,7 @@ filebrowser     = "doublecmd"
 terminal        = "/usr/bin/urxvt"
 editor          = os.getenv("EDITOR") or "editor"
 editor_cmd      = terminal .. " -e " .. editor
+vain.widgets.terminal = terminal
 
 beautiful.init(configpath.."/themes/zenburn-luxick/theme.lua")
 
@@ -25,7 +26,7 @@ beautiful.init(configpath.."/themes/zenburn-luxick/theme.lua")
 -- Table of layouts to cover with awful.layout.inc, order matters.
 layouts =
 {
-    lain.layout.uselesstile,
+    vain.layout.uselesstile,
     awful.layout.suit.tile,
     awful.layout.suit.max,
     --awful.layout.suit.floating
@@ -38,17 +39,18 @@ tags = {
 }
 -- Settings for the main screen
 tags[1] = awful.tag(tags.main, 1, layouts[1])
-awful.layout.set(lain.layout.uselessfair, tags[1][7])
+
+awful.layout.set(vain.layout.uselessfair, tags[1][7])
 awful.tag.setnmaster(2, tags[1][7])
 awful.tag.setncol(2, tags[1][7])
 
-awful.layout.set(awful.layout.suit.max, tags[1][8])
---awful.tag.setmwfact(0.5, tags[1][2])
---awful.tag.setncol(1, tags[1][7])
+awful.layout.set(vain.layout.browse, tags[1][8])
+awful.tag.setmwfact(0.5, tags[1][8])
+awful.tag.setncol(1, tags[1][8])
 
-awful.layout.set(awful.layout.suit.max, tags[1][9])
---awful.tag.setmwfact(0.5, tags[1][2])
---awful.tag.setncol(1, tags[1][8])
+awful.layout.set(vain.layout.browse, tags[1][9])
+awful.tag.setmwfact(0.5, tags[1][9])
+awful.tag.setncol(1, tags[1][9])
 
 -- Additional screens get tags from 1-9
 for s = 2, screen.count() do
